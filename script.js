f2e179b1d5c0c90450bd0caca10788ee75f13f23
Reuben411 <reubenondibamagaji11@gmail.com>
2025-06-17 09:47:22 +0100

    changed responsive menu function.

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-    //JavaScript for Toggle Menu//
+const hamburger = document.querySelector('.hamburger');
+const navlinks = document.querySelector('.nav-links');
+let menuOpen = false;
 
-
-     //   let navLinks = document.getElementById("navLinks");
-
-     //   function hideMenu(){
-     //       navLinks.style.right = "-200px";
-     //   }
-       //         function showMenu(){
-     //       navLinks.style.right = "0";
-      //  };
-
-
-function showMenu() {
-  const navLinks = document.getElementById("navLinks");
-  navLinks.style.display = "block";
-  navLinks.style.right = "0"; // Shows the menu (adjust based on your CSS)
-}
-
-function hideMenu() {
-  const navLinks = document.getElementById("navLinks");
-  //navLinks.style.right = "-200px"; // Hides the menu (adjust based on your CSS)
-    navLinks.style.display = "none"; // Or set 'right: -100%' for sliding menus
-
-}
-
-// Optional: Close menu when clicking a nav link
-document.querySelectorAll(".nav-links a").forEach(link => {
-  link.addEventListener("click", hideMenu);
+hamburger.addEventListner('click', () =>{
+  if (menuOpen == false) {
+    navlinks.style.display = "block";
+    menuOpen = true;
+  }
+  else if (menuOpen == true) {
+    navlinks.style.display = "none";
+    menuOpen = false ;
+  }
 });
-
-
